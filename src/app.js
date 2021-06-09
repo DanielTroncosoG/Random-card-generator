daa58d633a1d42a34cf377c3e763ac0b95e96b64
DanielTroncosoG <di.trgonzalez@gmail.com>
2021-06-09 20:17:54 +0000

    commit carta

--- src/app.js
+++ src/app.js
@@ -4,8 +4,55 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+function MazoCarta() {
+  const numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const pintas = ["Hearts", "Diamonds", "Spades", "Clubs"];
+  const cards = [];
+  for (let i = 0; i < pintas.length; i++) {
+    for (let c = 0; c < numeros.length; c++) {
+      const numero = numeros[c];
+      const pinta = pintas[i];
+      cards.push({ numero, pinta });
+    }
+  }
+  return cards;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function randomCard(cards) {
+  const random = Math.floor(Math.random() * 51);
+  const numerocarta = cards[random].numero;
+  const pintacarta = cards[random].pinta;
+  let dibujo;
+  pintacarta === "Diamonds"
+    ? (dibujo = "&diams;")
+    : (dibujo = "&" + pintacarta.toLowerCase() + ";");
+  const card = document.createElement("div");
+  card.classList.add("card", pintacarta.toLowerCase());
+  card.innerHTML =
+    '<span class="card-pinta top">' +
+    dibujo +
+    "</span>" +
+    '<span class="card-numero">' +
+    numerocarta +
+    "</span>" +
+    '<span class="card-pinta bot">' +
+    dibujo +
+    "</span>";
+  document.body.appendChild(card);
+}
+const cards = MazoCarta();
+randomCard(cards);
